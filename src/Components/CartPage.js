5eb7b6547d42f8cdfc3d519fbc653843c89e43d6
Mugil99 <mugilya99@gmail.com>
2023-06-01 23:23:22 +0530

    first commit

---
+++ src/Components/CartPage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { useDispatch,useSelector } from "react-redux";
+import { removeFromCart,checkout } from "../redux/actions/cartActions";
+import { useNavigate } from "react-router-dom";
+
+const CartPage=()=>{
+    const {cart}=useSelector(state=>state.cart);
+    const dispatch=useDispatch();
+    let navigate=useNavigate();
+    const totalPrice=cart.reduce((total,product)=>total+product.price,0)
+   
+    return(
+        <div>
+          <div className="container">  
+            {
+               cart.map(item=>(
+                <div key={item.id}>
+                   <h3>{item.title}</h3>
+                   <img src={item.thumbnail} alt={item.title} style={{width:"200px"}}/> 
+                            <p><strong>Description:</strong>{item.description}</p>
+                            <p><strong>₹{item.price}</strong></p>
+                            <button
+                              onClick = {() => dispatch(removeFromCart(item.id))}
+                            >Remove From Cart</button>
+                           
+                </div>
+                
+               ))
+            }
+            </div>
+            <h2>Total Price: {totalPrice}</h2>
+            <button onClick={()=>{dispatch(checkout()) }} style={{margin:"5px"}}>Checkout</button>
+            <button onClick={()=>navigate('/')} style={{margin:"5px"}}>Homepage</button>
+        
+        </div>
+    )
+}
+export default CartPage;
---
+++ src/Components/HomePage.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import { useDispatch,useSelector } from "react-redux";
+import { fetchProducts } from "../redux/actions/apiActions";
+import { addToCart } from "../redux/actions/cartActions";
+import {useNavigate} from 'react-router-dom';
+
+const HomePage=()=>{
+    let navigate=useNavigate();
+    const dispatch = useDispatch();
+    const{loading,products,error}=useSelector(state=>state.products);
+
+
+    useEffect(()=>{
+        dispatch(fetchProducts());
+    },[])
+
+    if(loading) return <h1>Loading...</h1>
+    if(error) return <h1>Error :{error}</h1>
+
+    return(
+        <div>
+            <button onClick={()=>navigate('/cart')}>Cart Page</button>
+            <div className="container">
+    {
+        products.map((product,index)=>(
+            
+            <div key={product.id}>
+                
+            <h3>{product.title}</h3>
+            <img src={product.thumbnail} alt={product.title} style={{width:"200px"}} />
+            <p><strong>Description:</strong> {product.description}</p>
+            <p><strong>₹{product.price}</strong></p>
+            <p><strong>Discount:</strong>{product.discountPercentage}</p>
+            <p><strong>Rating:</strong>{product.rating}</p>
+            <p><strong>stock:</strong>{product.stock}</p>
+            <p><strong>Brand:</strong>{product.brand}</p>
+            <p><strong>category:</strong>{product.category}</p>
+            <br></br>
+            <button onClick={()=>{dispatch(addToCart(product))}}>Add to Cart</button>
+           </div>
+        
+        ))
+    }
+        </div>        
+        </div>
+    )
+
+}
+export default HomePage;
